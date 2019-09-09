7be3586f8a93fbbb70ff66dd6323f3533c641272
mechgodzilla <sirena.chiang@sleepygiant.com>
2019-09-08 23:49:46 -0700

    missing question 5c and 10
    almost complete

--- homework/homework4 comments.cpp
+++ homework/homework4 comments.cpp
@@ -43,11 +43,13 @@ leave between the frame and the greeting.
 The framing program writes the mostly blank lines that separate the borders from
 the greeting one character at a time.Change the program so that it writes all the spaces
 needed in a single output expression.
-	 const std::string spaces(greeting.size(), ' ');
+	 note: use const std::string spaces(greeting.size(), ' ');
+	 see homework4 question4
 
 
 2 - 5.
 Write a set of "*" characters so that they form a square, a rectangle, and a triangle.
+	see homework4 question5
 
 
 2 - 6.
@@ -81,7 +83,7 @@ Write a program to generate the product of the numbers in the range[1, 10).
 
 2 - 9.
 Write a program that asks the user to enter two numbers and tells the user which number is larger than the other.
-	
+	see homework4 question9
 
 
 2 - 10.
--- homework/homework4.cpp
+++ homework/homework4.cpp
@@ -3,51 +3,71 @@
 
 using namespace std;
 
-void question1();
+void question4();
+void question5();
 void question7();
 void question8();
+void question9();
 
 int main() {
-	//question1();
+	//question4();
+	question5();
 	//question7();
-	question8();
+	//question8();
+	//question9();
 	return 0;
 }
 
 
 
-
-void question1() {
+void question4() {
 	cout << "Please enter your first name: ";
 	string name;
 	cin >> name;
 	const string greeting = "Hello, " + name + "!";
-	// const std::string spaces(greeting.size(), ' ');
 	const int pad = 1;
 	const int rows = pad * 2 + 3;
 	const int cols = greeting.size() + pad * 2 + 2;
 	cout << endl;
 
-	for (int r = 0; r != rows; ++r) {
-		for (int c = 0; c != cols; ++c) {
-			if (r == pad + 1 && c == pad + 1) {
-				cout << greeting;
-				c += greeting.size() - 1;
-			}
-			else {
-				if (r == 0 || r == rows - 1 || c == 0 || c == cols - 1) {
-					cout << "*";
-				}
-				else {
-					cout << " ";
-				}
-			}
+	int middle = rows - pad - 1;
+
+	for (int i = 1; i <= rows; i++) {
+		if (i == 1 || i == rows){
+			cout << string(cols, '*') << endl;
 		}
-		cout << endl;
+		else if (i == middle) {
+			cout << ('*') << string(pad, ' ')<< greeting  << string(pad, ' ') << ('*') << endl;
+		}
+		else {
+			cout << ('*') << string((cols-2), ' ') << ('*') << endl;
+		}
 	}
-	
 }
 
+void question5() {
+	int height = 4;
+	int width = 5;
+
+	//square
+		for (int i = 0; i < height; i++) {
+			cout << string(height, '*') << endl;
+	}
+
+	//rectangle
+		for (int i = 0; i < height; i++) {
+			cout << string(width, '*') << endl;
+		}
+
+	//triangle
+		for (int i = 0; i < width; i++) {
+			cout << string(i, ' ');
+			int astr = i * 2;
+			int subtract = width - astr;
+			cout << string(subtract, '*') << endl;
+	}
+
+}
 
 void question7() {
 	for (int i = 10; i > -6; i--) {
@@ -57,9 +77,30 @@ void question7() {
 }
 
 void question8() {
-	double multiple = 1;
+	int multiple = 1;
 	for (int i = 1; i <= 10; i++) {
 		multiple *= i;
 		cout << multiple << endl;
 	}
+}
+
+void question9() {
+	int firstint;
+	int secondint;
+	cout << "Enter first number:";
+	cin >> firstint;
+	cout << "Enter second number:";
+	cin >> secondint;
+	//double <15 float <7
+	if (firstint > secondint) {
+		cout << firstint << " is larger";
+	}
+	else if (secondint < firstint) {
+		cout << secondint << " is larger";
+	}
+	else if (secondint == firstint) {
+		cout << "they equal";
+	}
+
+
 }
